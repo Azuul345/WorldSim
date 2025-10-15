b82d078ff3ccd6f4657d406a6b845ebe2ae793af
Diego Oviedo <diego.oviedo@hotmail.se>
2025-10-15 16:40:26 +0200

    update

---
+++ ConsoleApp1/Human.cs
@@ -0,0 +1,263 @@
+﻿namespace ConsoleApp1
+{
+    public enum Gender { Male, Female, Other }
+
+
+    internal class Human
+    {
+        public static long Population = 0;
+
+        //core Identity 
+        //public string SurName { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public Guid DNA { get; set; }    //unik indetifier
+        public bool IsAlive { get; set; }
+
+
+        /// <summary>
+        /// biological & phisical
+        /// </summary>
+        //public double BMI { get; set; } // health
+        public double Health { get; set; } = 100;
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public double EnergyLevel { get; set; } = 100; //10
+
+        public double Strenght { get; set; }
+        public double Endurance { get; set; }
+
+        public double ImmuneSystem { get; set; }
+
+        public double SleepQuality { get; set; } = 100;
+        public double Hunger { get; set; } = 0.0; //full
+
+        public double Thirst { get; set; } = 0.0; //Full
+        public string BloodType { get; set; }
+
+
+        //Emotional & congnetive 
+        public double Intelligence { get; set; }
+
+        public double Empaty { get; set; }
+        public double Creativity { get; set; }
+        //public int Iq { get; set; }
+        //public int Eq { get; set; }
+
+        public double MemoryRetention { get; set; }
+        public double Focus { get; set; }
+        public double Curiosity { get; set; }
+        public double StressLevel { get; set; } = 0.0;
+        public double Wisdom { get; set; }
+        public double Happiness { get; set; } // 10
+
+
+        // Social
+        public List<Human> SocialConection { get; set; } = new List<Human>();
+        public List<string> Skills { get; set; } = new List<string>();
+        public double Reputation { get; set; }
+        public double Charisma { get; set; }
+        public double Integrity { get; set; }
+
+        //Economic survival 
+        public decimal Wealth { get; set; }
+        public string Occupation { get; set; }
+        public string EducationLevel { get; set; }
+        public string HomeLocation { get; set; }
+
+        //Existencial 
+        public string Purpuse { get; set; }
+        public bool IsReligious { get; set; }
+        public double FaithLevel { get; set; }
+        public double LegacyLevel { get; set; }
+
+
+        public Human(string name, Gender gender, string birthLocation, bool isAlaive)
+        {
+            Name = name;
+            Gender = gender;
+            Age = 0;
+            Intelligence = 20;
+            Empaty = 20;
+            Strenght = 5;
+            Creativity = 5;
+            DNA = Guid.NewGuid();
+            HomeLocation = birthLocation;
+
+            IsAlive = isAlaive;
+            Population++;
+        }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine($"Age: {Age}. Gender: {Gender}. Health: {Health}. Energy:{EnergyLevel} Happiness: {Happiness}");
+        }
+
+        public void Speak(Human human)
+        {
+            Console.WriteLine($"{Name} says HeLlO to {human.Name}");
+        }
+
+        public void Rest()
+        {
+            StressLevel -= 5;
+            Happiness += 5;
+            EnergyLevel += 5;
+        }
+
+        public void Work()
+        {
+            Wealth += 20;
+            EnergyLevel -= 10;
+            StressLevel += 10;
+        }
+
+        public void Sleep()
+        {
+            if (IsAlive)
+            {
+                EnergyLevel = 100;
+                SleepQuality = 100;
+
+            }
+        }
+
+        public void Eat()
+        {
+            Random rnd = new Random();
+            int hungerDecrease = rnd.Next(2, 5);
+            if (Hunger > 0)
+            {
+                Hunger -= hungerDecrease;
+                EnergyLevel += 3;
+                Happiness += 4;
+            }
+
+        }
+
+        public static Human RandomHuman()
+        {
+            Random rnd = new Random();
+            string[] maleNames = { "Johan", "Abi", "Hamza", "Lukas", "Oskar", "Christian", "Java Jack" };
+            string[] femaleNames = { "Emma", "Linda", "Yifan", "Zara", "Spitz", "Madona" };
+            string[] location = { "Majorna", "Angared", "Solna", "Uppsala", "Rosengard" };
+
+            Gender randomPeople;
+            int gendergenerator = rnd.Next(0, 2);
+            string randomName;
+            if (gendergenerator == 1)
+            {
+                randomPeople = Gender.Male;
+                randomName = maleNames[rnd.Next(maleNames.Length)];
+            }
+            else
+            {
+                randomPeople = Gender.Female;
+                randomName = femaleNames[rnd.Next(femaleNames.Length)];
+            }
+            string birthLocation = location[rnd.Next(location.Length)];
+            Human human = new(randomName, randomPeople, birthLocation, true);
+            return human;
+
+        }
+
+        public Human MakeChild(Human partner)
+        {
+            Random rnd = new Random();
+            string[] maleNames = { "Noah", "Cain", "Able", "Disable", "Frank", "Abraham" };
+            string[] femaleNames = { "Lilith", "Sheniqua", "Sara", "Eva", "Alina", "Beyonce" };
+
+
+            Gender Child;
+            int gendergenerator = rnd.Next(0, 2);
+            string childName;
+
+            if (gendergenerator == 1)
+            {
+                Child = Gender.Male;
+                childName = maleNames[rnd.Next(maleNames.Length)];
+            }
+            else
+            {
+                Child = Gender.Female;
+                childName = femaleNames[rnd.Next(maleNames.Length)];
+            }
+
+            Human human = new Human(childName, Child, partner.HomeLocation, true);
+            return human;
+
+        }
+
+        public static void RandomDeathInPopulation(List<Human> humans)
+        {
+            Random rnd = new Random();
+            foreach (var h in humans)
+            {
+                int chanceToDie = rnd.Next(5);
+                if (chanceToDie == 1)
+                {
+                    h.IsAlive = false;
+                    Population--;
+                }
+            }
+        }
+
+        public void Die()
+        {
+            IsAlive = false;
+            Population--;
+        }
+
+
+
+
+        public static void CountChildren(List<Human> humans)
+        {
+            var children = humans.Where(x => x.HomeLocation == "Garden of Eden" && x.Age < 20).ToList();
+
+
+            Console.WriteLine($"Amount of children: {children.Count}");
+        }
+
+        public void GetOlder()
+        {
+            Age++;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} Guid: {DNA} Gender: {Gender}. Age: {Age}. Is Alive: {IsAlive}. Birth Location: {HomeLocation}";
+        }
+
+
+
+
+        ////public string Proffession { get; set; }
+        //public string Race { get; set; }
+        //public string Language { get; set; }
+        ////public string Religion { get; set; }
+        //public string Nationality { get; set; }
+        //public string EyeColor { get; set; }
+        //public string HairColor { get; set; }
+        //public string TaxBracket { get; set; }
+        ////public string Education { get; set; }
+        ////public bool Hobo { get; set; }
+        //public string Sexuality { get; set; }
+        //public string Birthday { get; set; }
+        //public string ZodiacSign { get; set; }
+        //public bool Virginity { get; set; }
+        //public bool Athlete { get; set; }
+        //public bool Litteracy { get; set; }
+        //public bool HasHair { get; set; }
+        //public bool Autism { get; set; }
+        //public bool MentalDisorder { get; set; } //Lista kandidat
+        //public string Addiciton { get; set; } // Lista kandidat
+        //public string Hobby { get; set; } // Lista kandidat
+        //public bool Immigrant { get; set; }
+        //public bool Beneficiary { get; set; }
+        //public string Allergies { get; set; }
+        //public bool IsBreeder { get; set; }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@
             adam.Age = 35;
             eve.Age = 33;
 
-            Console.WriteLine("Begining of humanity");
+            Console.WriteLine("Beginning of humanity");
 
             List<Human> Soceity = new List<Human>();
             Soceity.Add(eve);
@@ -34,16 +34,138 @@
             Soceity[2].GetOlder();
             Soceity[2].GetOlder();
 
+            Soceity[4].GetOlder();
+            Soceity[4].GetOlder();
+            Soceity[4].GetOlder();
+            Soceity[4].GetOlder();
+            Soceity[4].GetOlder();
 
-            Console.WriteLine(Soceity[2].ToString());
+
+            //Console.WriteLine(Society[2].ToString());   test
+
+            // -------------1
+            //Console.WriteLine("All humans in the list");
+
+            //foreach (Human we in Soceity)
+            //{
+            //    Console.WriteLine(we.ToString());
+
+            //}
 
 
-            var peopleover10 = Soceity.Where(x => x.Age > 8).ToList();
+            //---------- 2
+            //Console.WriteLine($"Amount of humans in the world: {Human.Population}");
 
-            foreach (var person in peopleover10)
-            {
-                Console.WriteLine(person);
-            }
+
+            //----------- 3 
+            //Console.WriteLine("Female humans ==================");
+            //var females = Soceity.Where(f => f.Gender == Gender.Female).ToList();
+            //foreach (var f in females)
+            //{
+            //    Console.WriteLine(f);
+
+            //}
+
+
+            //---------- 4
+            //var peopleover10AndMale = Soceity.Where(x => x.Age > 8 && x.Gender == Gender.Male).ToList();
+
+            //foreach (var person in peopleover10AndMale)
+            //{
+            //    Console.WriteLine(person);
+            //}
+
+
+            //----------- 5
+            //Human.CountChildren(Soceity);
+
+            //--------- 6
+            //var oldest = Soceity.OrderByDescending(x => x.Age).ToList();
+            //Console.WriteLine(oldest[0].ToString());
+
+            //-------- 7 
+            //Console.WriteLine("After age loop");
+
+            //foreach (var h in Soceity)
+            //{
+            //    h.GetOlder();
+            //}
+            //foreach (var h in Soceity)
+            //{
+            //    Console.WriteLine(h.ToString());
+            //}
+
+
+
+            // ----------- 8 
+            //var avrageAge = Soceity.Average(e => e.Age);
+            //Console.WriteLine(avrageAge);
+
+            //------------ 9 
+            //Human.RandomDeathInPopulation(Soceity);
+            //Console.WriteLine("\n\n\n");
+            //foreach (var h in Soceity)
+            //{
+            //    Console.WriteLine(h.ToString());
+            //}
+            //Console.WriteLine(Human.Population);
+
+
+            //----------- 10
+            //var p3 = Soceity[2];
+            //Console.WriteLine($"{p3.Name} Hunger:{p3.Hunger} Energy: {p3.EnergyLevel}. Happiness: {p3.Happiness}");
+            //Console.WriteLine("\n\n\n");
+            //p3.Hunger = 20;
+            //p3.Eat();
+            //Console.WriteLine($"{p3.Name} Hunger:{p3.Hunger} Energy: {p3.EnergyLevel}. Happiness: {p3.Happiness}");
+
+
+            // ----------- 11
+            //var p3 = Soceity[2];
+            //p3.EnergyLevel = 80;
+            //p3.SleepQuality = 80;
+            //p3.Sleep();
+            //Console.WriteLine($"Name:{p3.Name} Energy: {p3.EnergyLevel}. Sleep Quality: {p3.SleepQuality}");
+
+            //-------------- 16
+            //var ages = Soceity.OrderByDescending(a => a.Age);
+            //foreach (var p in ages)
+            //{
+            //    Console.WriteLine(p.ToString());
+            //}
+
+
+            //-------------- 17
+            //var p1 = Human.RandomHuman();
+            //var p2 = Human.RandomHuman();
+            //var p3 = Human.RandomHuman();
+            //Console.WriteLine(p1.ToString());
+            //Console.WriteLine(p2.ToString());
+            //Console.WriteLine(p3.ToString());
+
+            //-------------- 18
+            //var groups = Soceity.GroupBy(h => h.Gender).ToList();
+            //foreach (var h in groups[0])
+            //{
+            //    Console.WriteLine(h.ToString());
+            //}
+            //Console.WriteLine("\n\n\n");
+            //foreach (var h in groups[1])
+            //{
+            //    Console.WriteLine(h.ToString());
+            //}
+
+
+            // -------------- 19
+
+
+
+
+
+
+
+
+
 
 
             //var saras = Soceity.Where(x => x.Name == "Sara").ToList();
@@ -55,22 +177,6 @@
 
 
 
-
-
-
-
-
-
-
-
-            //Console.WriteLine("All humans in the list");
-
-            //foreach (Human we in Soceity)
-            //{
-            //    Console.WriteLine(we.ToString());
-
-            //}
-
             //Console.WriteLine("Male humans ===================");
             //foreach (Human h in Soceity)
             //{
@@ -79,15 +185,6 @@
             //        Console.WriteLine(h);
             //    }
             //}
-
-            //Console.WriteLine("Female humans ==================");
-            //var females = Soceity.Where(f => f.Gender == Gender.Female).ToList();
-            //foreach (var f in females)
-            //{
-            //    Console.WriteLine(f);
-
-            //}
-
 
 
             //linq search and filter list / databases 
